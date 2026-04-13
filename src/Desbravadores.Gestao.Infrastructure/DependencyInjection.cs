0a7d7b3deaccd18d9efa25b210ef8b3025599e4a
Lucas Souza <lucas.rsouza@yduqs.com.br>
2026-04-13 10:45:58 -0300

    Ajustando Postgress #3

---
+++ src/Desbravadores.Gestao.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Desbravadores.Gestao.Application.Interfaces;
+using Desbravadores.Gestao.Infrastructure.Data;
+using Desbravadores.Gestao.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Desbravadores.Gestao.Infrastructure;
+
+public static class DependencyInjection
+{
+  public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+  {
+    services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(
+          Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+          ?? throw new InvalidOperationException("Connection string 'DefaultConnection' não configurada."))
+        );
+
+    services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+    return services;
+  }
+}
