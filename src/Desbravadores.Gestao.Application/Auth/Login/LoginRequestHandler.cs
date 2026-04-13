6fd035b270355571b8b3178e07554ff3460066c3
Lucas Souza <lucas.rsouza@yduqs.com.br>
2026-04-13 00:13:37 -0300

    Ajuste no validador de senhas

--- src/Desbravadores.Gestao.Application/Auth/Login/LoginRequestHandler.cs
+++ src/Desbravadores.Gestao.Application/Auth/Login/LoginRequestHandler.cs
@@ -8,17 +8,23 @@ namespace Desbravadores.Gestao.Application.Auth.Login;
 public sealed class LoginRequestHandler(
     IUsuarioRepository usuarioRepository,
     IUsuarioSessaoRepository usuarioSessaoRepository,
-    ITokenService tokenService)
+    ITokenService tokenService,
+    IPasswordHasher passwordHasher)
 {
   private readonly IUsuarioRepository _usuarioRepository = usuarioRepository;
   private readonly IUsuarioSessaoRepository _usuarioSessaoRepository = usuarioSessaoRepository;
   private readonly ITokenService _tokenService = tokenService;
+  private readonly IPasswordHasher _passwordHasher = passwordHasher;
 
   public async Task<LoginResponse> HandleAsync(LoginRequest request, CancellationToken cancellationToken = default)
   {
-    var usuario = await _usuarioRepository.GetByEmailAsync(request.Email, cancellationToken) 
-    ?? throw new UnauthorizedAccessException("E-mail ou senha inválidos.");
+    var usuario = await _usuarioRepository.GetByEmailAsync(request.Email, cancellationToken)
+      ?? throw new UnauthorizedAccessException("E-mail ou senha inválidos.");
 
+    var senhaValida = await _passwordHasher.VerifyAsync(request.Senha, usuario.Senha, cancellationToken);
+
+    if (!senhaValida)
+      throw new UnauthorizedAccessException("E-mail ou senha inválidos.");
 
     TokenResult token = await _tokenService.GenerateToken(usuario);
 
@@ -33,9 +39,9 @@ public sealed class LoginRequestHandler(
     await _usuarioSessaoRepository.SaveChangesAsync(cancellationToken);
 
     return new LoginResponse(
-        token,
-        token.AccessTokenExpiraEm,
-        usuario.Nome,
-        usuario.Email);
+      token,
+      token.AccessTokenExpiraEm,
+      usuario.Nome,
+      usuario.Email);
   }
 }
